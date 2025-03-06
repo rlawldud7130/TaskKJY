5227f76d3e606bc6069b80585553b37635237ed3
김지영 <rlawldud7130@naver.com>
2025-03-07 04:14:16 +0900

    총알 판정 ray로 바꿔서 동시에 여러마리 맞는거 방지, 안맞는거 방지

--- Assets/2.Script/Objeck/Bullet.cs
+++ Assets/2.Script/Objeck/Bullet.cs
@@ -7,13 +7,42 @@ public class Bullet : MonoBehaviour
     public int damage = 10;
     public LayerMask hitLayer;
 
-    //총알이 몬스터 때리기
-    private void OnTriggerEnter2D(Collider2D other)
+    private Rigidbody2D rb;
+    private Vector2 previousPosition;
+
+    private void Awake()
     {
-        if ((hitLayer.value & (1 << other.gameObject.layer)) != 0)
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void Start()
+    {
+        previousPosition = rb.position;
+    }
+
+    //레이로 총알 쏜거 맞는판정 확인
+    private void FixedUpdate()
+    {
+        Vector2 currentPosition = rb.position;
+        Vector2 direction = currentPosition - previousPosition;
+        float distance = direction.magnitude;
+
+        if (distance > 0)
         {
-            other.GetComponent<MonsterManager>().Hit(damage);
-            Destroy(this.gameObject);
+            RaycastHit2D hit = Physics2D.Raycast(previousPosition, direction.normalized, distance, hitLayer);
+            if (hit.collider != null)
+            {
+                MonsterManager monsterManager = hit.collider.GetComponent<MonsterManager>();
+                if (monsterManager != null)
+                {
+                    monsterManager.Hit(damage);
+                }
+
+                Destroy(gameObject);
+                return;
+            }
         }
+
+        previousPosition = currentPosition;
     }
 }
