943456e60952c7c06d46aac9c6837faf000b2b24
김지영 <rlawldud7130@naver.com>
2025-03-08 02:08:43 +0900

    플레이어 공격 공격속도제한, 총알판정 수정. 완료

--- Assets/2.Script/Hero/HeroAttack.cs
+++ Assets/2.Script/Hero/HeroAttack.cs
@@ -4,12 +4,14 @@ using UnityEngine;
 
 public class HeroAttack : MonoBehaviour
 {
+    [SerializeField] private float attackSpeed;
     public GameObject bulletPrefab;
     public float bulletSpeed = 10f;
     private Rigidbody2D rb;
     private GameObject bullet;
     private Animator animator;
 
+    private float attackTimer = 0.0f;
 
     private void Start()
     {
@@ -24,22 +26,32 @@ public class HeroAttack : MonoBehaviour
     //ÃÑ½î±â
     private void FireBullet()
     {
+        if (attackTimer < 1.0f)
+        {
+            attackTimer += Time.deltaTime * attackSpeed;
+        }
+
         if (Input.GetMouseButtonDown(0))
         {
-            Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-            mousePos.z = 0f;
-
-            Vector2 direction = (mousePos - transform.position).normalized;
-
-            bullet = Instantiate(bulletPrefab, transform.position, Quaternion.identity);
-            rb = bullet.GetComponent<Rigidbody2D>();
-
-            if (rb != null)
+            if(attackTimer > 1.0f)
             {
-                rb.velocity = direction * bulletSpeed;
-            }
+                attackTimer = 0.0f;
 
-            animator.SetTrigger("Attack");
+                Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+                mousePos.z = 0f;
+
+                Vector2 direction = (mousePos - transform.position).normalized;
+
+                bullet = Instantiate(bulletPrefab, transform.position, Quaternion.identity);
+                rb = bullet.GetComponent<Rigidbody2D>();
+
+                if (rb != null)
+                {
+                    rb.velocity = direction * bulletSpeed;
+                }
+
+                animator.SetTrigger("Attack");
+            }
         }
     }
 }
